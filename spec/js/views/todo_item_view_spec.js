0b6fbd369a31c95847aac4c8a3f24da7da71b41f
OvidiuBan <ovfl.ban@gmail.com>
2015-04-14 15:08:38 +0300

    [#6] delete items

--- spec/js/views/todo_item_view_spec.js
+++ spec/js/views/todo_item_view_spec.js
@@ -1,12 +1,15 @@
 define([
   'views/todo_item_view',
   'model/todo',
+  'collection/todos',
   'ejs'
-], function (ToDoItemView, ToDo) {
-  var instance, subject, model;
+], function (ToDoItemView, ToDo, ToDos) {
+  var instance, subject, model, collection;
 
   beforeEach(function () {
     model = new ToDo({title: 'nu'});
+    collection = new ToDos();
+    collection.add(model);
     instance = new ToDoItemView({model: model});
     subject = function () {
       return instance;
@@ -43,6 +46,40 @@ define([
     it('will render a button', function () {
       subject();
       expect(instance.$('input[type="button"]').length).toEqual(1);
+    });
+  });
+
+  describe('events', function () {
+    beforeEach(function () {
+      instance.render();
+    });
+
+    describe('click delete button', function () {
+      beforeEach(function () {
+        subject = function () {
+          instance.$('.btnDelete').click();
+        }
+      });
+
+      it('will remove the model from the collection', function () {
+        subject();
+        expect(collection.length).toEqual(0);
+      });
+
+      describe('view has parent', function () {
+        var $parent;
+
+        beforeEach(function () {
+          $parent = $('<div></div>');
+          $parent.html(instance.$el);
+        });
+
+        it('will remove the rendered view', function () {
+          subject();
+          expect($.contains($parent.get(0), instance.el)).toBe(false);
+
+        })
+      });
     });
   });
 });
