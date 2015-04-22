1032cafc250133cf04c9414575e4bd7440db656b
calin <calinoiu.alexandru@agilefreaks.com>
2015-04-22 20:17:37 +0300

    [#17] extracted todo view from todo list view

--- app/js/views/todos_list_view.js
+++ app/js/views/todos_list_view.js
@@ -2,19 +2,25 @@ define([
   'lodash',
   'jquery',
   'backbone',
-  'text!templates/todos_list_view.ejs'
+  'text!templates/todos_list_view.ejs',
+  'views/todo_view'
 
-], function (_, $, Backbone, todosTemplate) {
+], function (_, $, Backbone, todosTemplate, TodoView) {
   return Backbone.View.extend({
+    tagName: 'ul',
 
-    initialize: function() {
-      this.listenTo(this.collection, "add", this.render, this);
+    onNewModel: function(model){
+      this.todoItem = new TodoView({ model: model });
+      this.$el.append(this.todoItem.render().$el);
     },
 
     render: function () {
-      var compiledTemplate = ejs.render(todosTemplate, {collection: this.collection}, {});
+      var compiledTemplate = ejs.render(todosTemplate, {}, {});
       this.$el.empty();
+
       this.$el.append(compiledTemplate);
+      this.listenTo(this.collection, 'add', this.onNewModel, this);
+
       return this;
     }
   });
--- spec/js/views/todos_list_view_spec.js
+++ spec/js/views/todos_list_view_spec.js
@@ -1,12 +1,14 @@
 define([
   'views/todos_list_view',
-  'collections/todos'
-], function (ListView, Todos) {
+  'views/todo_view',
+  'collections/todos',
+  'models/todo'
+], function (ListView, TodoView, Todos, Todo) {
   var instance, subject, collection;
 
   beforeEach(function () {
     collection = new Todos();
-    instance = new ListView({ collection: collection});
+    instance = new ListView({collection: collection});
     subject = function () {
       return instance;
     };
@@ -19,10 +21,40 @@ define([
       }
     });
 
-    it ('render todos list', function () {
+    it('render todos list', function () {
+      var renderTodoViewSpy = spyOn(instance, 'render').and.callThrough();
+
       subject();
 
-      expect(instance.$('ul').length).toEqual(1);
+      expect(renderTodoViewSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('adding a new item to the collection', function () {
+    beforeEach(function () {
+
+      var todo = new Todo({title: 'my todo'});
+      subject = function () {
+        collection.push(todo);
+      }
+    });
+
+    describe('the view has been rendered', function () {
+      var $renderedEl;
+      beforeEach(function () {
+        instance.render();
+
+        $renderedEl = $('<div id="someId"></div>');
+        spyOn(TodoView.prototype, 'render').and.returnValue({
+          $el: $renderedEl
+        });
+      });
+
+      it('renders a corresponding view for the item', function () {
+        subject();
+
+        expect($.contains(instance.$el[0], $renderedEl[0])).toBe(true);
+      });
     });
   });
 });
