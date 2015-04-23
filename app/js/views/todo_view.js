69643193a0c52a935dab5b2f8241140cd026fe84
Florin <florin.cabuz@yahoo.com>
2015-04-23 11:32:04 +0300

    [#11] improved style

--- app/js/views/todo_view.js
+++ app/js/views/todo_view.js
@@ -6,9 +6,21 @@ define([
 
 ], function (_, $, Backbone, todoTemplate) {
   return Backbone.View.extend({
-
     tagName: 'li',
     className: 'list-group-item',
+
+    events: {
+      'mouseover': 'showDeleteButton',
+      'mouseout': 'hideDeleteButton'
+    },
+
+    showDeleteButton: function () {
+      this.$('button').addClass('show');
+    },
+
+    hideDeleteButton: function () {
+      this.$('button').removeClass('show');
+    },
 
     render: function () {
       var compiledTemplate = ejs.render(todoTemplate, {model: this.model}, {});
