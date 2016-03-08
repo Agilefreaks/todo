ddd345906e3554aa9a9405622d728674a608ba78
Catrina Marius <mcatrina14@gmail.com>
2016-03-08 14:32:54 +0200

    add events handlers for click and key down

--- app/js/views/app_view.js
+++ app/js/views/app_view.js
@@ -1,21 +1,39 @@
 define([
   'jquery',
   'backbone',
-  'text!templates/app_view.ejs'
-], function ($, Backbone, indexTemplate) {
+  'text!templates/app_view.ejs',
+  'collections/todo'
+], function ($, Backbone, indexTemplate, todoCollection) {
   return Backbone.View.extend({
-    el: this.$('#todo-app'),
+    el: $('#todo-app'),
+    input: $('#input'),
 
-    currentDate: function () {
-      return new Date();
+    events: {
+      'click #addButton': 'addItems',
+      'keydown #input': 'keyAction'
     },
 
-    render: function () {
-      var compiledTemplate = ejs.render(indexTemplate, {view: this, model: this.model}, {});
+    addItems: function(){
+      var newItem = input.value;
+      if(_.trim(newItem) !== ""){
+        this.collection.add({name: newItem, done: false});
+      }
+      input.value = "";
+    },
 
+    keyAction: function(e){
+      if(e.which === 13){
+        this.addItems();
+      }
+    },
+
+
+    render: function () {
+      this.collection = new todoCollection();
+      var compiledTemplate = ejs.render(indexTemplate, {view: this, model: this.model}, {});
       this.$el.empty();
       this.$el.append(compiledTemplate);
-      return this;
+      return this
     }
   });
 });
