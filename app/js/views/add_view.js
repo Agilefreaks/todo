367984bd2ec9ec1f4e10fde21df68079c2406296
Catrina Marius <mcatrina14@gmail.com>
2016-03-11 17:39:21 +0200

    render views, when we add a todo the view for a todo is rendered but i can't share the collection data, so isn't display the right the name

--- app/js/views/add_view.js
+++ app/js/views/add_view.js
@@ -1,14 +1,20 @@
-//var KEY_ENTER_CODE = 13;
+var KEY_ENTER_CODE = 13;
 
 define([
   'jquery',
   'backbone',
-  'text!templates/add_view.ejs'
-], function ($, Backbone, addTemplate) {
+  'text!templates/add_view.ejs',
+  'models/todo',
+  'collections/todos'
+], function ($, Backbone, addTemplate, Todo, Todos) {
   return Backbone.View.extend({
     events: {
       'click #addButton': 'onAddButtonClick',
       'keydown #input': 'onKeyDown'
+    },
+
+    initialize: function () {
+      this.todos = new Todos();
     },
 
     el: this.$('#todoAdd'),
@@ -22,30 +28,30 @@ define([
     },
 
     onAddButtonClick: function () {
-      //var input = this.$('#input');
+      var input = this.$('#input');
 
-      //this.addTodo(input.val());
-      //input.val('');
+      this.addTodo(input.val());
+      input.val('');
     },
 
-    addTodo: function () {
-      //var newTodo = new Todo();
-//
-      //if (_.isEmpty(_.trim(input))) {
-      //  return;
-      //}
-      //newTodo.set('name', input);
-      //newTodo.set('done', false);
-      //this.todos.push(newTodo);
-      //this.renderTodos(this.$('#todoList'));
+    addTodo: function (input) {
+      var newTodo = new Todo();
+
+      if (_.isEmpty(_.trim(input))) {
+        return;
+      }
+      newTodo.set('name', input);
+      newTodo.set('done', false);
+      this.todos.push(newTodo);
+      this.trigger('addTodo', this.todos);
     },
 
-    onKeyDown: function () {
-      //if (e.which !== KEY_ENTER_CODE) {
-      //  return;
-      //}
-      //e.preventDefault();
-      //this.onAddButtonClick();
+    onKeyDown: function (e) {
+      if (e.which !== KEY_ENTER_CODE) {
+        return;
+      }
+      e.preventDefault();
+      this.onAddButtonClick();
     }
   });
 });
--- app/js/views/app_view.js
+++ app/js/views/app_view.js
@@ -1,5 +1,3 @@
-var KEY_ENTER_CODE = 13;
-
 define([
   'jquery',
   'backbone',
@@ -7,27 +5,29 @@ define([
   'text!templates/todo_view.ejs',
   'models/todo',
   'collections/todos',
-   'views/add_view',
+  'views/add_view',
   'views/todo_view'
 ], function ($, Backbone, indexTemplate, todoTemplate, Todo, Todos, AddView, TodoView) {
   return Backbone.View.extend({
     initialize: function () {
-      this.todos = new Todos();
+      this.addView = new AddView();
+      this.todoView = new TodoView();
+      this.listenTo(this.addView, 'addTodo', this.renderTodo);
     },
 
     el: this.$('#todo-app'),
 
     render: function () {
       var compiledTemplate = ejs.render(indexTemplate, {view: this, model: this.model}, {});
-	  var addView = new AddView();
-	  var todoView = new TodoView();
 
       this.$el.empty();
       this.$el.append(compiledTemplate);
-	  addView.setElement(this.$('#todoAdd')).render();
-	  todoView.setElement(this.$('#todoList')).render();
-	  
+      this.addView.setElement(this.$('#todoAdd')).render();
       return this;
+    },
+
+    renderTodo: function () {
+      this.todoView.setElement(this.$('#todoList')).render();
     }
   });
 });
