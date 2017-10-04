ac929e8882e2ab4d3baddedd1290653e5a47121b
Constantin Sorin Capatina <constantin.capatina@visma.com>
2017-10-05 00:21:47 +0300

    Add todo item logic

--- app/js/views/app_view.js
+++ app/js/views/app_view.js
@@ -1,13 +1,32 @@
 define([
   'jquery',
   'backbone',
-  'text!templates/app_view.ejs'
-], function ($, Backbone, indexTemplate) {
+  'text!templates/app_view.ejs',
+  'collections/todo_item_collection',
+  'models/todo_item'
+], function ($, Backbone, indexTemplate, TodoItemCollection, TodoItem) {
   return Backbone.View.extend({
     el: this.$('#todo-app'),
+    initialize: function(){
+      this.todoItemCollection = new TodoItemCollection();
+    },
+
+    events:{
+      "click #add-todo-button" : "addTodo"
+    },
 
     currentDate: function () {
       return new Date();
+    },
+
+    addTodo: function() {
+      var todoTitle = this.$('#todo-title-textbox').val();
+      if(!todoTitle) return;
+      var todoItem = new TodoItem({
+        title: todoTitle,
+      });
+      this.todoItemCollection.add(todoItem);
+      console.log(todoItem.get('title'));
     },
 
     render: function () {
